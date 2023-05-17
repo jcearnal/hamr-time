f41638240c66332385ccceb83c63ae84b3d6f7ca
tromaville <jcearnal@gmail.com>
2023-05-17 19:24:22 -0400

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import AuthScreen from './screens/AuthScreen';
 import TestAdmin from './screens/TestAdmin';
+import backgroundImage from './assets/background_img.png';
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center top', // Adjusted property
+    minHeight: '100vh',
+    width: '100%',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* Your existing code */}
+    <div style={styles.container}>
       <TestAdmin />
-    </View>
+    </div>
   );
-  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/TestAdmin.js
+++ screens/TestAdmin.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react';
 import './TestAdmin.css';
 import Countdown from './Countdown';
 import ScoreScreen from './ScoreScreen';
+import backgroundImage from '../assets/background_img.png';
 
 const TestAdmin = () => {
   const testData = [
-    { level: 1, shuttles: 7, speed: 8.0, shuttleTime: 9.0, totalTime: 63.0, distance: 140 },
-    { level: 2, shuttles: 8, speed: 9.0, shuttleTime: 8.0, totalTime: 64.0, distance: 160 },
-    { level: 3, shuttles: 8, speed: 9.5, shuttleTime: 7.58, totalTime: 60.63, distance: 160 },
-    { level: 4, shuttles: 9, speed: 10.0, shuttleTime: 7.20, totalTime: 64.80, distance: 180 },
-    { level: 5, shuttles: 9, speed: 10.5, shuttleTime: 6.86, totalTime: 61.71, distance: 180 },
-    { level: 6, shuttles: 10, speed: 11.0, shuttleTime: 6.55, totalTime: 65.50, distance: 200 },
-    { level: 7, shuttles: 10, speed: 11.5, shuttleTime: 6.26, totalTime: 62.61, distance: 200 },
-    { level: 8, shuttles: 11, speed: 12.0, shuttleTime: 6.00, totalTime: 66.00, distance: 220 },
-    { level: 9, shuttles: 11, speed: 12.5, shuttleTime: 5.76, totalTime: 63.36, distance: 220 },
-    { level: 10, shuttles: 11, speed: 13.0, shuttleTime: 5.54, totalTime: 60.92, distance: 220 },
-    { level: 11, shuttles: 12, speed: 13.5, shuttleTime: 5.33, totalTime: 64.00, distance: 240 },
-    { level: 12, shuttles: 12, speed: 14.0, shuttleTime: 5.14, totalTime: 61.71, distance: 240 },
-    { level: 13, shuttles: 13, speed: 14.5, shuttleTime: 4.97, totalTime: 64.55, distance: 260 },
-    { level: 14, shuttles: 13, speed: 15.0, shuttleTime: 4.80, totalTime: 62.40, distance: 260 },
-    { level: 15, shuttles: 13, speed: 15.5, shuttleTime: 4.65, totalTime: 60.39, distance: 260 },
-    { level: 16, shuttles: 14, speed: 16.0, shuttleTime: 4.50, totalTime: 63.00, distance: 280 },
-    { level: 17, shuttles: 14, speed: 16.5, shuttleTime: 4.36, totalTime: 61.09, distance: 280 },
-    { level: 18, shuttles: 15, speed: 17.0, shuttleTime: 4.24, totalTime: 63.53, distance: 300 },
-    { level: 19, shuttles: 15, speed: 17.5, shuttleTime: 4.11, totalTime: 61.71, distance: 300 },
-    { level: 20, shuttles: 16, speed: 18.0, shuttleTime: 4.00, totalTime: 64.00, distance: 320 },
-    { level: 21, shuttles: 16, speed: 18.5, shuttleTime: 3.89, totalTime: 62.27, distance: 320 },
-    ];
+    { level: 1, shuttles: 7, speed: 8.0, shuttleTime: 9.0 },
+    { level: 2, shuttles: 8, speed: 9.0, shuttleTime: 8.0 },
+    { level: 3, shuttles: 8, speed: 9.5, shuttleTime: 7.58 },
+    { level: 4, shuttles: 9, speed: 10.0, shuttleTime: 7.20 },
+    { level: 5, shuttles: 9, speed: 10.5, shuttleTime: 6.86 },
+    { level: 6, shuttles: 10, speed: 11.0, shuttleTime: 6.55 },
+    { level: 7, shuttles: 10, speed: 11.5, shuttleTime: 6.26 },
+    { level: 8, shuttles: 11, speed: 12.0, shuttleTime: 6.00 },
+    { level: 9, shuttles: 11, speed: 12.5, shuttleTime: 5.76 },
+    { level: 10, shuttles: 11, speed: 13.0, shuttleTime: 5.54 },
+    { level: 11, shuttles: 12, speed: 13.5, shuttleTime: 5.33 },
+    // { level: 12, shuttles: 12, speed: 14.0, shuttleTime: 5.14 },
+    // { level: 13, shuttles: 13, speed: 14.5, shuttleTime: 4.97 },
+    // { level: 14, shuttles: 13, speed: 15.0, shuttleTime: 4.80 },
+    // { level: 15, shuttles: 13, speed: 15.5, shuttleTime: 4.65 },
+    // { level: 16, shuttles: 14, speed: 16.0, shuttleTime: 4.50 },
+    // { level: 17, shuttles: 14, speed: 16.5, shuttleTime: 4.36 },
+    // { level: 18, shuttles: 15, speed: 17.0, shuttleTime: 4.24 },
+    // { level: 19, shuttles: 15, speed: 17.5, shuttleTime: 4.11 },
+    // { level: 20, shuttles: 16, speed: 18.0, shuttleTime: 4.00 },
+    // { level: 21, shuttles: 16, speed: 18.5, shuttleTime: 3.89 },
+  ];
 
   const [currentShuttle, setCurrentShuttle] = useState({ level: 1, iteration: 1 });
   const [timeRemaining, setTimeRemaining] = useState(testData[0].shuttleTime);
@@ -74,9 +76,34 @@ const TestAdmin = () => {
     
       
   
+      // const handleNextShuttle = () => {
+      //   const currentLevel = currentShuttle.level;
+      //   const currentIteration = currentShuttle.iteration;
+      
+      //   if (currentIteration === testData[currentLevel - 1].shuttles) {
+      //     if (currentLevel === testData.length) {
+      //       console.log('Test Completed');
+      //       // Add logic to handle the completion of the test
+      //     } else {
+      //       setCurrentShuttle({ level: currentLevel + 1, iteration: 1 });
+      //       setTimeRemaining(testData[currentLevel].shuttleTime); // Changed from currentLevel to currentLevel - 1
+      //       setCumulativeTime((prevTime) => prevTime + testData[currentLevel - 1].shuttleTime);
+      //     }
+      //   } else {
+      //     setCurrentShuttle({ level: currentLevel, iteration: currentIteration + 1 });
+      //     setTimeRemaining(testData[currentLevel - 1].shuttleTime); // Removed -1
+      //     setCumulativeTime((prevTime) => prevTime + testData[currentLevel - 1].shuttleTime);
+      //   }
+      // };     
       const handleNextShuttle = () => {
         const currentLevel = currentShuttle.level;
         const currentIteration = currentShuttle.iteration;
+      
+        if (currentLevel === 11 && currentIteration === 12) {
+          console.log('Maximum points reached. Test Completed');
+          stopTest(); // Call the stopTest function to stop the test
+          return;
+        }
       
         if (currentIteration === testData[currentLevel - 1].shuttles) {
           if (currentLevel === testData.length) {
@@ -84,15 +111,16 @@ const TestAdmin = () => {
             // Add logic to handle the completion of the test
           } else {
             setCurrentShuttle({ level: currentLevel + 1, iteration: 1 });
-            setTimeRemaining(testData[currentLevel].shuttleTime); // Changed from currentLevel to currentLevel - 1
+            setTimeRemaining(testData[currentLevel].shuttleTime);
             setCumulativeTime((prevTime) => prevTime + testData[currentLevel - 1].shuttleTime);
           }
         } else {
           setCurrentShuttle({ level: currentLevel, iteration: currentIteration + 1 });
-          setTimeRemaining(testData[currentLevel - 1].shuttleTime); // Removed -1
+          setTimeRemaining(testData[currentLevel - 1].shuttleTime);
           setCumulativeTime((prevTime) => prevTime + testData[currentLevel - 1].shuttleTime);
         }
-      };     
+      };
+      
       
     
 
@@ -125,7 +153,7 @@ const TestAdmin = () => {
       setCurrentShuttle({ level: 1, iteration: 1 });
       setCumulativeTime(0);
       setTimeRemaining(testData[0].shuttleTime);
-      setCountdown(5);
+      setCountdown(3);
     };
   
     const stopTest = () => {
@@ -142,40 +170,57 @@ const TestAdmin = () => {
       setCountdown(0);
     };
 
+    const ProgressBar = ({ progress }) => {
+      return (
+        <div className="progress-bar">
+          <div className="progress-bar-fill" style={{ width: `${progress}%` }} />
+        </div>
+      );
+    };
+
     
 
     return (
-        <div className='test-admin'>
-          <h2>HAMR Time</h2>
-          {countdown > 0 ? (
-            <p>Countdown: {countdown}</p>
-          ) : (
-            <>
-              {testStarted && !testStopped && (
-                <>
-                  <p>Time Remaining: {timeRemaining.toFixed(2)}</p>
-                  <p>
-                    Current Shuttle: Level {currentShuttle.level}, Iteration {currentShuttle.iteration}
-                  </p>
-                  <p>Cumulative Time: {formatTime(cumulativeTime)}</p>
-                  <button onClick={stopTest}>Stop Test</button>
-                </>
-              )}
-              {!testStarted && !testStopped && (
-                <button onClick={startTest}>Start Test</button>
-              )}
-              {testStopped && (
-                <div>
-                  <h2>Test Stopped</h2>
-                  <p>Last Completed Shuttle: Level {currentShuttle.level}, Iteration {currentShuttle.iteration - 1}</p>
-                  <button onClick={restartTest}>Restart Test</button>
+      <div className='test-admin' style={{ textAlign: 'center' }}>
+        <h1>HAMR Time</h1>
+        {countdown > 0 ? (
+          <p>Countdown: {countdown}</p>
+        ) : (
+          <>
+            {testStarted && !testStopped && (
+              <>
+                <div className="progress-bar">
+                  <div
+                    className="progress-bar-fill"
+                    style={{ width: `${(timeRemaining / testData[currentShuttle.level - 1].shuttleTime) * 100}%` }}
+                  />
                 </div>
-              )}
-            </>
-          )}
-        </div>
-      );
-        
+                <h1 className='green'>{timeRemaining.toFixed(2)}</h1>
+                <p>Current Shuttle:</p>
+                  
+                <h3>Level {currentShuttle.level}, Shuttle {currentShuttle.iteration}</h3>
+                
+                {/* <p>Total Time - Last Completed Shuttle <br></br> {formatTime(cumulativeTime)}</p> */}
+                <button onClick={stopTest}>Stop Test</button>
+              </>
+            )}
+            {!testStarted && !testStopped && (
+              <button onClick={startTest}>Start Test</button>
+            )}
+            {testStopped && (
+              <div>
+                <h2 className={testStopped ? 'test-stopped' : ''}>Test Complete!</h2>
+                <p>Great work!</p>
+                <p>Final Score: <strong>Level {currentShuttle.level}, Shuttle {currentShuttle.iteration - 1}</strong></p>
+                <p>Total Shuttles Completed: {currentShuttle.level * (currentShuttle.iteration - 1)}</p>
+    
+                <button onClick={restartTest}>Restart Test</button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    ); 
   
 };
 
